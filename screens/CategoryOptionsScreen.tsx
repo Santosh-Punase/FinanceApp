038d519985d3571dd2081a2c582478761a253778
Santosh-Punase <santoshpunase@gmail.com>
2022-11-20 17:58:04 +0530

    ability to delete category

--- screens/CategoryOptionsScreen.tsx
+++ screens/CategoryOptionsScreen.tsx
@@ -1,6 +1,6 @@
 import { FontAwesome } from '@expo/vector-icons';
 import { useState } from 'react';
-import { ActivityIndicator, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import { ActivityIndicator, Alert, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { Input } from '../components/Input';
 
 import { View, Text } from '../components/Themed';
@@ -17,6 +17,23 @@ export default function CategoryOptionsScreen({ navigation, route }: any) {
 
   // @ts-ignore
   const parsedCategories: Category[] = categories !== '' ? parseObject(categories) : [];
+
+  const createTwoButtonAlert = (category: string) =>
+    Alert.alert(
+      `Delete ${category} ?`,
+      "Are you sure? This can't be undone.",
+      [
+        {
+          text: "Cancel",
+          style: "cancel"
+        },
+        { text: "YES",
+          style: 'destructive',
+          onPress: () => setCategory(stringifyObject(parsedCategories.filter((c) => c.name !== category )))
+        }
+      ],
+      { cancelable: true, }
+    );
 
   const updateTimestamp = (c: Category) => {
     const updatedCategories = parsedCategories.map(ct => ct.name === c.name ? c : ct);
@@ -60,9 +77,14 @@ export default function CategoryOptionsScreen({ navigation, route }: any) {
         ) : (
           <View style={[{ width: '100%', paddingTop: 10, }]}>
             {parsedCategories.filter((c: Category) => c.name.toLowerCase().includes(searchString.toLowerCase())).sort((a,b) => b.timestamp - a.timestamp).map((c: Category, i: number) => (
-              <TouchableOpacity style={[styles.row, { borderRadius: 8, }]} key={i} onPress={() => onSelect(c)}>
-                <Text style={styles.category}>{c.name}</Text>
-              </TouchableOpacity>
+              <View style={styles.listItem} key={i} >
+                <TouchableOpacity onPress={() => onSelect(c)}>
+                  <Text style={styles.category}>{c.name}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => createTwoButtonAlert(c.name)}>
+                  <Text style={styles.closeIcon}>+</Text>
+                </TouchableOpacity>
+              </View>
             ))}
             <TouchableOpacity style={[styles.row, { borderRadius: 8, }]} onPress={onAddNew}>
               <Text style={[styles.category, { color: 'blue' }]}> + Add {searchString || 'New'}</Text>
@@ -87,10 +109,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginBottom: 10,
   },
+  listItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+    borderRadius: 8,
+  },
+  closeIcon: {
+    paddingHorizontal: 16,
+    paddingVertical: 5,
+    height: 50,
+    textAlignVertical: 'center',
+    fontSize: 30,
+    color: 'red',
+    transform: [{ rotateZ: '45deg' }]
+  },
   category: {
     paddingHorizontal: 16,
     paddingVertical: 5,
     height: 50,
+    width: Layout.window.width - 80,
     textAlignVertical: 'center',
   },
 });
