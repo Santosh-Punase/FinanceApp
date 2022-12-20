888b6b0352f77a0edd551de0dcda07c02caae551
Santosh-Punase <santoshpunase@gmail.com>
2022-12-20 15:05:37 +0530

    error handling and shake animation in input modal

--- components/Modals/InputModal.tsx
+++ components/Modals/InputModal.tsx
@@ -1,6 +1,6 @@
 import { AntDesign } from "@expo/vector-icons";
 import React, { useRef, useState } from "react";
-import { StyleSheet, TouchableOpacity, Modal, Platform, TextInput, KeyboardAvoidingView } from "react-native";
+import { StyleSheet, TouchableOpacity, Modal, Platform, KeyboardAvoidingView, Animated } from "react-native";
 import { Input } from "../Input";
 import { View, Text } from "../Themed";
 import { modalStyles } from "./modal.styles";
@@ -17,13 +17,37 @@ type Props = {
 
 export function InputModal({ title, onSubmit, initialValue = '', onCancel, onChangeText, placeholder }: Props) {
   const [value, setValue] = useState<string>(initialValue);
+  const [errored, setErrored] = useState<boolean>(false);
+  const [animation] = useState(new Animated.Value(0));
 
   const _onChangeText = (value: string) => {
+    _setError(value);
 		setValue(value);
 		onChangeText?.(value);
 	}
 
+  const _setError = (value: string) => {
+    if(!value) {
+      setErrored(true);
+    } else {
+      setErrored(false);
+    }
+  }
+
+  const startShake = () => {
+    Animated.sequence([
+      Animated.timing(animation, { toValue: 10, duration: 100, useNativeDriver: true }),
+      Animated.timing(animation, { toValue: -10, duration: 100, useNativeDriver: true }),
+      Animated.timing(animation, { toValue: 10, duration: 100, useNativeDriver: true }),
+      Animated.timing(animation, { toValue: 0, duration: 100, useNativeDriver: true })
+    ]).start();
+ }
+
 	const _onSubmit = () => {
+    if(errored) {
+      startShake();
+      return;
+    } 
 		onSubmit(value);
     setValue('');
 	}
@@ -52,14 +76,16 @@ export function InputModal({ title, onSubmit, initialValue = '', onCancel, onCha
             </View>
 
             <View style={{ paddingVertical: 10 }}>
-              <Input
-                value={value}
-                autoFocus
-                placeholder={placeholder}
-                style={styles.textInput}
-                onChangeText={_onChangeText}
-                onSubmitEditing={_onSubmit}
-              />
+              <Animated.View style={{ transform: [{ translateX: animation }] }}>
+                <Input
+                  value={value}
+                  autoFocus
+                  placeholder={placeholder}
+                  style={[errored ? { ...styles.textInput, borderColor: 'red' } : styles.textInput]}
+                  onChangeText={_onChangeText}
+                  onSubmitEditing={_onSubmit}
+                />
+              </Animated.View>
             </View>
             
             <ModalFooter cancelText={'cancel'} submitText={'save'} onSubmit={_onSubmit} onCancel={_onCancel} />
