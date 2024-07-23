80ef1f8bd0865fc2819b75e8dd541ad28eafcc41
santosh punase <santoshpunase@gmail.com>
2024-07-23 16:35:22 +0530

    Added monthly budget card

--- components/Home/styles.tsx
+++ components/Home/styles.tsx
@@ -3,6 +3,9 @@ import { WIDTH } from "../../constants/Layout";
 
 export const styles = StyleSheet.create({
   cardStyle: { marginTop: 10, marginHorizontal: 10, width: WIDTH - 20, flexDirection: 'column' },
+  cardItem: { flexDirection: 'row', justifyContent: 'space-between', width: '100%', marginBottom: 5, },
+  cardHeader: { fontSize: 16 },
+  cardSubHeader: { fontSize: 14 },
   header: {
     fontSize: 16,
     marginVertical: 10,
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,9 @@
 import { StyleSheet, ColorSchemeName } from 'react-native';
 
-import { Text, View } from '../components/Themed';
+import { View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
-import { Card } from '../components/Card';
-import { HEIGHT, WIDTH } from '../constants/Layout';
+// import { Card } from '../components/Card';
+import { HEIGHT } from '../constants/Layout';
 import { useTheme } from '../theme';
 import { Categories } from '../components/Home/Categories';
 import { RecentTransactions } from '../components/Home/RecentTransactions';
@@ -14,22 +14,12 @@ export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>)
 
   return (
     <View style={styles.container} lightColor='rgba(255,255,255, 0.5)' darkColor='rgba(255, 255, 255, 0.1)'>
-      <Card style={styles.cardStyle}>
+      {/* <Card style={styles.cardStyle}>
         <View style={styles.cardItem}>
           <Text style={{ fontSize: 18 }}>Net Balance</Text>
           <Text style={{ fontSize: 16 }}>0</Text>
         </View>
-      </Card>
-      <Card style={styles.cardStyle}>
-        <View style={styles.cardItem}>
-          <Text>Title</Text>
-          <Text>Title</Text>
-        </View>
-        <View style={styles.cardItem}>
-          <Text>Title 2</Text>
-          <Text>Title 2</Text>
-        </View>
-      </Card>
+      </Card> */}
       <Categories navigation={navigation} currentTheme={currentTheme} />
       <RecentTransactions navigation={navigation} />
     </View>
@@ -49,8 +39,8 @@ const styles = StyleSheet.create({
   //   fontWeight: 'bold',
   //   alignSelf: 'flex-start',
   // },
-  cardStyle: { marginTop: 10, marginHorizontal: 10, width: WIDTH - 20, flexDirection: 'column' },
-  cardItem: { flexDirection: 'row', justifyContent: 'space-between', width: '100%' },
+  // cardStyle: { marginTop: 10, marginHorizontal: 10, width: WIDTH - 20, flexDirection: 'column' },
+  // cardItem: { flexDirection: 'row', justifyContent: 'space-between', width: '100%' },
   // separator: {
   //   marginVertical: 30,
   //   height: 1,
